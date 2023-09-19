3c4a1b91b0f5565942a71f80d21b3375206fa485
Imani Niyigena <imanix8@gmail.com>
2023-09-19 18:53:20 +0200

    Table adjustments based on business logic

--- tables.sql
+++ tables.sql
@@ -13,7 +13,7 @@ CREATE TABLE SeedBreederVendor (
 -- Create the Genetic Marker Table
 CREATE TABLE GeneticMarker (
     GeneticMarkerID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    Genus VARCHAR(100) NOT NULL,  -- Added NOT NULL constraint
+    Genus VARCHAR(100) NOT NULL,
     Species VARCHAR(100) UNIQUE NOT NULL
 );
 
@@ -39,37 +39,16 @@ CREATE TABLE StrainSeedBag (
     StrainSeedBagID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     StrainSeedBagName VARCHAR(150) NOT NULL,,
     PackageUnits INT NOT NULL,
-    DateReceived DATE DEFAULT GETDATE() NOT NULL,
+    DateReceived DATE GETDATE() DEFAULT NOT NULL,
     StrainID INT NOT NULL,
-    BreederVendorID INT NOT NULL,
     FOREIGN KEY (StrainID) REFERENCES Strain(StrainID)
-      ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (BreederVendorID) REFERENCES SeedBreederVendor(BreederVendorID)
       ON UPDATE CASCADE ON DELETE CASCADE
 );
-
--- -- Create the Phenotypic Marker Table
--- CREATE TABLE PhenotypicMarker (
---     PhenotypicMarkerID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
---     MarkerName VARCHAR(100) UNIQUE NOT NULL,
---     GeneticMarkerID INT NOT NULL,
---     NumberOfBranches INT,
---     Height INT,
---     LeafColour VARCHAR(100),  -- Adjusted length based on your data
---     FOREIGN KEY (GeneticMarkerID) REFERENCES GeneticMarker(GeneticMarkerID)
---       ON UPDATE CASCADE ON DELETE CASCADE
--- );
 
 -- Create the Seed Table
 CREATE TABLE Seed (
     SeedID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    BreederVendorID INT NOT NULL,
-    GeneticMarkerID INT NOT NULL,
     StrainSeedBagID INT NOT NULL,
-    FOREIGN KEY (BreederVendorID) REFERENCES SeedBreederVendor(BreederVendorID)
-        ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (GeneticMarkerID) REFERENCES GeneticMarker(GeneticMarkerID)
-        ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (StrainSeedBagID) REFERENCES StrainSeedBag(StrainSeedBagID)
         ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -78,7 +57,7 @@ CREATE TABLE Seed (
 CREATE TABLE Seeding (
     SeedingID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     SeedID INT NOT NULL,
-    DatePlanted DATE DEFAULT GETDATE() NOT NULL,
+    DatePlanted DATE GETDATE() DEFAULT NOT NULL,
     FOREIGN KEY (SeedID) REFERENCES Seed(SeedID)
       ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -87,7 +66,7 @@ CREATE TABLE Seeding (
 CREATE TABLE Seedling (
     SeedlingID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     SeedID INT NOT NULL,
-    SproutDate DATE DEFAULT GETDATE() NOT NULL,
+    SproutDate DATE GETDATE() DEFAULT NOT NULL,
     HealthStatus BOOLEAN NOT NULL,
     FOREIGN KEY (SeedID) REFERENCES Seed(SeedID)
       ON UPDATE CASCADE ON DELETE CASCADE
@@ -97,16 +76,8 @@ CREATE TABLE Seedling (
 CREATE TABLE Mothers (
     MotherID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     SeedlingID INT NOT NULL,
-    DateMothered DATE DEFAULT GETDATE() NOT NULL,
-    NumberOfBranches INT NOT NULL,
-    LeafColour VARCHAR(150) NOT NULL,
-    GeneticMarkerID INT NOT NULL,
-    StrainID INT NOT NULL,
+    DateMothered DATE GETDATE() DEFAULT NOT NULL,
     FOREIGN KEY (SeedlingID) REFERENCES Seedling(SeedlingID)
-      ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (GeneticMarkerID) REFERENCES GeneticMarker(GeneticMarkerID)
-      ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (StrainID) REFERENCES Strain(StrainID)
       ON UPDATE CASCADE ON DELETE CASCADE
 );
 
@@ -114,10 +85,9 @@ CREATE TABLE Mothers (
 CREATE TABLE Maturity (
     MaturityID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     MotherID INT NOT NULL,
-    DateOfMaturityCheck DATE DEFAULT GETDATE() NOT NULL,
+    DateOfMaturityCheck DATE GETDATE() DEFAULT NOT NULL,
     Height INT NOT NULL,
     NumberOfBranches INT NOT NULL,
-    LeafColour VARCHAR(150) NOT NULL,
     FOREIGN KEY (MotherID) REFERENCES Mothers(MotherID)
       ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -126,19 +96,17 @@ CREATE TABLE Maturity (
 CREATE TABLE Cutting (
     CutID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     MaturityID INT NOT NULL,
-    CutDate DATE DEFAULT GETDATE() NOT NULL,
+    CutDate DATE GETDATE() DEFAULT NOT NULL,
     NumberOfCuts SMALLINT NOT NULL,
     FOREIGN KEY (MaturityID) REFERENCES Maturity(MaturityID)
       ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- Create the Transplant Table
---  MotherID INT NOT NULL,
 CREATE TABLE Transplant (
     TransplantID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     CutID INT NOT NULL,
-    TransplantDate DATE DEFAULT GETDATE() NOT NULL,
-    HealthStatus BOOLEAN NOT NULL,
+    TransplantDate DATE GETDATE() DEFAULT NOT NULL,
     FOREIGN KEY (CutID) REFERENCES Cutting(CutID)
       ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -147,16 +115,10 @@ CREATE TABLE Transplant (
 CREATE TABLE Daughter (
     DaughterID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     TransplantID INT UNIQUE NOT NULL,
-    MotherID INT NOT NULL,
-    GeneticMarkerID INT NOT NULL,
     Price SMALLINT NOT NULL,
-    Packaged BOOLEAN NOT NULL,
-    DateDaughtered DATE DEFAULT GETDATE() NOT NULL,
-    LeafColour VARCHAR(150) NOT NULL,
+    DateDaughtered DATE GETDATE() DEFAULT NOT NULL,
     HealthStatus BOOLEAN NOT NULL,
     Fate VARCHAR(50) NOT NULL, -- To track fate (e.g., 'Kept', 'Sold', 'Destroyed')
     FOREIGN KEY (TransplantID) REFERENCES Transplant(TransplantID)
-      ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (GeneticMarkerID) REFERENCES GeneticMarker(GeneticMarkerID)
       ON UPDATE CASCADE ON DELETE CASCADE
 );
