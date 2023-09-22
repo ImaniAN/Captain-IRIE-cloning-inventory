fe88815efa0b04dac521488898b21fb2c41b4c18
Imani Niyigena <imanix8@gmail.com>
2023-09-22 10:14:10 +0200

    Supabase PLSQL syntax compliance

--- tables.sql
+++ tables.sql
@@ -4,130 +4,118 @@ CREATE DATABASE ProjectMotherShip;
 -- Use the newly created database
 USE ProjectMotherShip;
 
--- Create the Seed Breeder Vendor Table
-CREATE TABLE SeedBreederVendor (
-    BreederVendorID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    VendorName VARCHAR(150) UNIQUE NOT NULL
-);
--- Table Explanation: Stores information about seed breeder vendors.
+create table
+  SeedBreederVendor (
+    BreederVendorID bigint primary key generated always as identity,
+    VendorName text not null unique
+  );
 
--- Create the Genetic Marker Table
-CREATE TABLE GeneticMarker (
-    GeneticMarkerID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    Genus VARCHAR(100) NOT NULL,
-    Species VARCHAR(100) UNIQUE NOT NULL
-);
 -- Table Explanation: Stores genetic marker information.
+create table
+  GeneticMarker (
+    GeneticMarkerID bigint primary key generated always as identity,
+    Genus text not null,
+    Species text not null unique
+  );
 
--- Create the Strain Table
-CREATE TABLE Strain (
-    StrainID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    NickName VARCHAR(150),
-    FirstName VARCHAR(150),
-    MiddleName VARCHAR(150),
-    LastName VARCHAR(150),
-    BreederVendorID INT NOT NULL,
-    GeneticMarkerID INT NOT NULL,
-    FOREIGN KEY (BreederVendorID) REFERENCES SeedBreederVendor(BreederVendorID)
-      ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (GeneticMarkerID) REFERENCES GeneticMarker(GeneticMarkerID)
-      ON UPDATE CASCADE ON DELETE CASCADE
-);
 -- Table Explanation: Represents different plant strains, linking them to vendors and genetic markers.
+create table
+  Strain (
+    StrainID bigint primary key generated always as identity,
+    NickName text,
+    FirstName text,
+    MiddleName text,
+    LastName text,
+    BreederVendorID bigint not null,
+    GeneticMarkerID bigint not null,
+    foreign key (BreederVendorID) references SeedBreederVendor (BreederVendorID) on update cascade on delete cascade,
+    foreign key (GeneticMarkerID) references GeneticMarker (GeneticMarkerID) on update cascade on delete cascade
+  );
 
--- Create the StrainSeedBag Table
-CREATE TABLE StrainSeedBag (
-    StrainSeedBagID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    StrainSeedBagName VARCHAR(150) NOT NULL,
-    PackageUnits INT NOT NULL,
-    DateReceived DATE GETDATE() DEFAULT NOT NULL,
-    StrainID INT NOT NULL,
-    FOREIGN KEY (StrainID) REFERENCES Strain(StrainID)
-      ON UPDATE CASCADE ON DELETE CASCADE
-);
 -- Table Explanation: Tracks seed bags for specific strains.
+create table
+  StrainSeedBag (
+    StrainSeedBagID bigint primary key generated always as identity,
+    StrainSeedBagName text not null,
+    PackageUnits smallint not null,
+    DateReceived timestamp with time zone default now() not null,
+    StrainID bigint not null,
+    foreign key (StrainID) references Strain (StrainID) on update cascade on delete cascade
+  );
 
--- Create the Seed Table
-CREATE TABLE Seed (
-    SeedID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    StrainSeedBagID INT NOT NULL,
-    FOREIGN KEY (StrainSeedBagID) REFERENCES StrainSeedBag(StrainSeedBagID)
-        ON UPDATE CASCADE ON DELETE CASCADE
-);
 -- Table Explanation: Represents individual seeds and their association with seed bags.
+create table
+  Seed (
+    SeedID bigint primary key generated always as identity,
+    StrainSeedBagID bigint not null,
+    foreign key (StrainSeedBagID) references StrainSeedBag (StrainSeedBagID) on update cascade on delete cascade
+  );
 
--- Create the Seeding Table
-CREATE TABLE Seeding (
-    SeedingID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    SeedID INT NOT NULL,
-    DatePlanted DATE GETDATE() DEFAULT NOT NULL,
-    FOREIGN KEY (SeedID) REFERENCES Seed(SeedID)
-      ON UPDATE CASCADE ON DELETE CASCADE
-);
 -- Table Explanation: Records the planting of seeds.
+create table
+  Seeding (
+    SeedingID bigint primary key generated always as identity,
+    SeedID bigint not null,
+    DatePlanted timestamp with time zone default now() not null,
+    foreign key (SeedID) references Seed (SeedID) on update cascade on delete cascade
+  );
 
--- Create the Seedling Table
-CREATE TABLE Seedling (
-    SeedlingID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    SeedID INT NOT NULL,
-    SproutDate DATE GETDATE() DEFAULT NOT NULL,
-    FOREIGN KEY (SeedID) REFERENCES Seed(SeedID)
-      ON UPDATE CASCADE ON DELETE CASCADE
-);
 -- Table Explanation: Tracks the growth of seedlings.
+create table
+  Seedling (
+    SeedlingID bigint primary key generated always as identity,
+    SeedID bigint not null,
+    SproutDate timestamp with time zone default now() not null,
+    foreign key (SeedID) references Seed (SeedID) on update cascade on delete cascade
+  );
 
--- Create the Mothers Table
-CREATE TABLE Mothers (
-    MotherID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    SeedlingID INT NOT NULL,
-    DateMothered DATE GETDATE() DEFAULT NOT NULL,
-    FOREIGN KEY (SeedlingID) REFERENCES Seedling(SeedlingID)
-      ON UPDATE CASCADE ON DELETE CASCADE
-);
 -- Table Explanation: Represents mature plants (mothers) obtained from seedlings.
+create table
+  Mothers (
+    MotherID bigint primary key generated always as identity,
+    SeedlingID bigint not null,
+    DateMothered timestamp with time zone default now() not null,
+    foreign key (SeedlingID) references Seedling (SeedlingID) on update cascade on delete cascade
+  );
 
--- Create the Maturity Table
-CREATE TABLE Maturity (
-    MaturityID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    MotherID INT NOT NULL,
-    DateOfMaturityCheck DATE GETDATE() DEFAULT NOT NULL,
-    Height INT NOT NULL,
-    NumberOfBranches INT NOT NULL,
-    FOREIGN KEY (MotherID) REFERENCES Mothers(MotherID)
-      ON UPDATE CASCADE ON DELETE CASCADE
-);
 -- Table Explanation: Records information about the maturity of mother plants.
+create table
+  Maturity (
+    MaturityID bigint primary key generated always as identity,
+    MotherID bigint not null,
+    DateOfMaturityCheck timestamp with time zone default now() not null,
+    height smallint not null,
+    NumberOfBranches int not null,
+    foreign key (MotherID) references Mothers (MotherID) on update cascade on delete cascade
+  );
 
--- Create the Cutting Table
-CREATE TABLE Cutting (
-    CutID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    MaturityID INT NOT NULL,
-    CutDate DATE GETDATE() DEFAULT NOT NULL,
-    NumberOfCuts SMALLINT NOT NULL,
-    FOREIGN KEY (MaturityID) REFERENCES Maturity(MaturityID)
-      ON UPDATE CASCADE ON DELETE CASCADE
-);
 -- Table Explanation: Tracks the cutting of mature plants.
+create table
+  Cutting (
+    CutID bigint primary key generated always as identity,
+    MaturityID bigint not null,
+    CutDate timestamp with time zone default now() not null,
+    NumberOfCuts smallint not null,
+    foreign key (MaturityID) references Maturity (MaturityID) on update cascade on delete cascade
+  );
 
--- Create the Transplant Table
-CREATE TABLE Transplant (
-    TransplantID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    CutID INT NOT NULL,
-    TransplantDate DATE GETDATE() DEFAULT NOT NULL,
-    FOREIGN KEY (CutID) REFERENCES Cutting(CutID)
-      ON UPDATE CASCADE ON DELETE CASCADE
-);
 -- Table Explanation: Records transplanting events.
+create table
+  Transplant (
+    TransplantID bigint primary key generated always as identity,
+    CutID bigint not null,
+    TransplantDate timestamp with time zone default now() not null,
+    foreign key (CutID) references Cutting (CutID) on update cascade on delete cascade
+  );
 
--- Create the Daughter Table
-CREATE TABLE Daughter (
-    DaughterID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-    TransplantID INT UNIQUE NOT NULL,
-    Price SMALLINT NOT NULL,
-    DateDaughtered DATE GETDATE() DEFAULT NOT NULL,
-    HealthStatus BOOLEAN NOT NULL,
-    Fate VARCHAR(50) NOT NULL, -- To track fate (e.g., 'Kept', 'Sold', 'Destroyed')
-    FOREIGN KEY (TransplantID) REFERENCES Transplant(TransplantID)
-      ON UPDATE CASCADE ON DELETE CASCADE
-);
 -- Table Explanation: Represents daughter plants obtained from transplants and tracks their details.
+create table
+  Daughter (
+    DaughterID bigint primary key generated always as identity,
+    TransplantID bigint not null unique,
+    Price smallint not null,
+    DateDaughtered timestamp with time zone default now() not null,
+    HealthStatus boolean not null,
+    Fate text not null,
+    foreign key (TransplantID) references Transplant (TransplantID) on update cascade on delete cascade
+  );
