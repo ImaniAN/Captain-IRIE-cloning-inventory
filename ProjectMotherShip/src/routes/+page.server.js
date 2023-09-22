30ace906f654d9d9fbae79635cff2742197fef50
Imani Niyigena <imanix8@gmail.com>
2023-09-22 18:44:53 +0200

    error removal

--- ProjectMotherShip/src/routes/+page.server.js
+++ ProjectMotherShip/src/routes/+page.server.js
@@ -4,9 +4,5 @@ export async function load() {
   const { data } = await supabase.from("strain").select();
   return {
 	strains: data ?? [],
-
-	const { data1 } = await supabase.from("strain").select();
-	return {
-	  strains: data1 ?? [],
   };
 }
