a12f1afa77b70a896e75a74be3775f2015282532
Imani Niyigena <imanix8@gmail.com>
2023-09-23 17:00:15 +0200

    Capitilzation of word Irie => IRIE

--- ProjectMotherShip/src/routes/+page.svelte
+++ ProjectMotherShip/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1 class="h1">Welcome to the Captain Irie Farms</h1>
+		<h1 class="h1">Welcome to the Captain IRIE Farms</h1>
 		<h1 class="h1">Cloning Inventory Management System!</h1>
 		<p>Start by exploring:()</p>
 		<ul>
